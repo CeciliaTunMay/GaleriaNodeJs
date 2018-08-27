ef2eccc2d902d162d07024ad89d6814ddc9a6e9d
Daryl Segovia Alonzo <daryl_segovia@hotmail.com>
2018-08-26 23:17:47 -0500

    Cambios

--- GaleriaNode/models/conexion.js
+++ GaleriaNode/models/conexion.js
@@ -4,7 +4,6 @@ port = process.env.PORT || 4205;
 if (port === 4205) {
     var connection = mysql.createConnection({
         host: 'den1.mysql1.gear.host',
-        //port: 3306,
         user: 'galeria',
         password: 'Ml0e!~74JX3C',
         database: 'galeria',
